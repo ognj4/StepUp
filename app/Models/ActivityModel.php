739031bd2bf707b4f3f999a7f3c24b78533c83f5
ognj4 <stepanovicognjenos@gmail.com>
2025-03-27 19:33:41 +0100

    * Model and migration for acitivity

---
+++ app/Models/ActivityModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ActivityModel extends Model
+{
+    protected $table = 'activity';
+    protected $fillable = ['name','description','duration','date'];
+}
